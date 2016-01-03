3fd3b624972023d98366febb4b7a9d4faadd619e
Daniel Slowinski <dan@classkick.com>
2016-01-03 11:16:37 -0800

    Added web portal

--- cloud/main.js
+++ cloud/main.js
@@ -2,6 +2,30 @@ var moment = require('moment');
 
 require('cloud/app.js');
 
+Parse.Cloud.afterSave("CallEvent",function(request) {
+	var randomNumber = getRandom();
+	Parse.Cloud.httpRequest({
+	  method: 'PUT',
+	  url: 'https://lola-help.firebaseio.com/.json',
+	  body: {
+	    callEvents: randomNumber
+	  }
+	}).then(function(httpResponse) {
+	  console.log(httpResponse.text);
+	}, function(httpResponse) {
+	  console.error('Request failed with response code ' + httpResponse.status);
+	});
+});
+
 // var Story = Parse.Object.extend("Story");
 // var Metadata = Parse.Object.extend("Metadata");
 
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandom() {
+  return getRandomInt(getRandomInt(0,1000),getRandomInt(5000,100000));
+}
